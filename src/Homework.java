e53ba69f35486b8006fcf5fe723e0e43c9e3abb0
Denis <demaz@bk.ru>
2022-09-27 12:06:49 +0700

    task 2 done

--- src/Homework.java
+++ src/Homework.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class Homework {
 
@@ -7,6 +7,21 @@ public class Homework {
     public void printOnlyOddNumbers(List nums) {
         for (Object num : nums) {
             if ((Integer) num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public void printOnlyOneCopyEvenNumbers(List nums) {
+        Set<Integer> setNumbers = new HashSet<>();
+        for (Object num : nums) {
+            setNumbers.add((Integer) num);
+        }
+        List sortedList = new ArrayList(setNumbers);
+        Collections.sort(sortedList);
+        for (Object num : sortedList) {
+            if ((Integer) num % 2 == 0) {
                 System.out.print(num + " ");
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Homework homework = new Homework(new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7)));
         homework.printOnlyOddNumbers(homework.getNums());
+        homework.printOnlyOneCopyEvenNumbers(homework.getNums());
     }
 
 
